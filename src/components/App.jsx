4861b717a8b292fe6748e7e6c1709a96d7939697
galpalex <galpalex@gmail.com>
2021-12-18 19:42:40 +0200

    dice inits

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="player">test</div>{" "}
+      </div>
+    );
+  }
+}
+
+export default App;
